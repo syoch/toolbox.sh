d18771f02f1089dad926744f99d12ad48f2bf64c
syoch <hacksyoch@gmail.com>
2022-07-20 00:41:00 +0900

    update: move some sources

--- src/_entry.sh
+++ src/_entry.sh
@@ -1,21 +1,13 @@
 #include "_config.sh"
-#include "logging.sh"
-#include "utils.sh"
 
-#include "command_loader.sh"
-#include "meta_opt_parser.sh"
-#include "opt_parser.sh"
-#include "embed_file.sh"
-
+#include "lib/lib.sh"
+#include "loaders/loaders.sh"
 #include "commands/commands.sh"
 
 function main {
   local subcommand=$1
   shift
 
-  load_options
-
-  parse_args $@
 
   if [ -z "$subcommand" ]; then
     subcommand="help"
---
+++ src/loaders/loaders.sh
@@ -0,0 +1,7 @@
+#include "meta_opt_parser.sh"
+#include "opt_parser.sh"
+
+function load_datas {
+  load_options
+  parse_args $@
+}
